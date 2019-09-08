3fa9ce22e505c9c8bc4a6ed5ba44ed10fc852958
Rupert Amodia <rupertraphael@live.com>
2019-09-08 13:50:48 -0600

    Add catch all route that redirects to root.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Catch all route
+Route::get('/{all}', function () {
+    return redirect('/');
+});
+
 Route::get('/api/organizations', 'OrganizationController@index');
 
 Route::get('/api/connections', 'ConnectionController@index');
